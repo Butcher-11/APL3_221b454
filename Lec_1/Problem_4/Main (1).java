71759bd2da86b1435441f7fd271466a850a26409
221B454 <221B454@juet.co.in>
2025-01-23 14:36:54 +0530

    Added all the problems of lecture 1

---
+++ Lec_1/Problem_4/Main (1).java
@@ -0,0 +1,7 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Inside the one class");
+		One obj = new One(5);
+	}
+}
---
+++ Lec_1/Problem_4/One.java
@@ -0,0 +1,9 @@
+class One {
+    int x;
+
+    One(int x) {
+        this.x = x;
+        System.out.println("Inside the Constructor "+ x);
+        System.out.println("Value passed "+ x);
+    }
+}
